d8c0a4f7a7af7e385eaebe1246f2000550d99c94
SarwatJahan22 <procontent22@gmail.com>
2025-01-03 02:09:45 +0500

    changes in blogcard

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import {Card, CardContent, CardTitle} from "../components/ui/card";
+import Image from 'next/image';
 
 
 
 
 interface BlogCardProps {
-  post:{id: string; title: string; description: string; date:string; imageUrl: string};
+  post:{id: string; title: string; description: string; date:string; image: string};
   isDarkBackground: boolean;
 }
 
 const BlogCard = ({post, isDarkBackground}: BlogCardProps) => {
   return (
    <Card className={`p-4 ${isDarkBackground ? 'bg-slate-800 text-white' : 'text-slate-800'} rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300`}>
-   /* eslint-disable @next/next/no-img-element */
-  <img
-  src={post.imageUrl}
-  alt={post.title}
-  className='w-full h-48 object-cover rounded-t-lg'/>
-  /* eslint-disable @next/next/no-img-element */
+ 
+ <Image 
+  src={post.image} 
+  alt={post.title} 
+  width={800} 
+  height={500} 
+  className="w-full h-auto"
+/>
+ 
   <CardTitle className='text-xl font-nomral mt-4 text-center'>{post.title}</CardTitle> 
 <br />
 <CardContent className='text-center'>
